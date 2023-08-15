46a5006a7b51d59f062b1f67eef63438cfe32525
Ariel Fogel <fogeltine@gmail.com>
2023-08-15 08:08:03 +0300

    fix colors and flash so that they work as expected

--- app/helpers/tailwind_helper.rb
+++ app/helpers/tailwind_helper.rb
@@ -1,10 +1,10 @@
 module TailwindHelper
   def tailwind_class_for(flash_type)
     {
-      success: "bg-green-100 border-green-700 text-green-700 hover:bg-green-200",
-      error: "bg-red-100 border-red-700 text-red-700 hover:bg-red-200",
-      alert: "bg-yellow-100 border-yellow-700 text-yellow-700 hover:bg-yellow-200",
-      notice: "bg-primary-100 border-primary-700 text-base-content-700 hover:bg-primary-200"
+      success: "success-flash hover:success-flash-hover",
+      error: "error-flash hover:error-flash-hover",
+      alert: "warn-flash hover:warn-flash-hover",
+      notice: "notice-flash hover:notice-flash-hover"
     }.stringify_keys[flash_type.to_s] || flash_type.to_s
   end
 end
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,5 +15,46 @@ module.exports = {
   ],
   daisyui: {
     themes: ["emerald"],
+    themes: [
+      {
+        emerald: {
+          ...require("daisyui/src/theming/themes")["[data-theme=emerald]"],
+          primary: "#45aeeb",
+          "primary-focus": "#1d94da",
+          ".success-flash": {
+            "background-color": colors.green[100],
+            "border-color": colors.green[700],
+            color: colors.green[700],
+          },
+          ".success-flash-hover": {
+            "background-color": colors.green[200],
+          },
+          ".error-flash": {
+            "background-color": colors.red[100],
+            "border-color": colors.red[700],
+            color: colors.red[700],
+          },
+          ".error-flash-hover": {
+            "background-color": colors.red[200],
+          },
+          ".warn-flash": {
+            "background-color": colors.amber[100],
+            "border-color": colors.amber[700],
+            color: colors.amber[700],
+          },
+          ".warn-flash-hover": {
+            "background-color": colors.amber[200],
+          },
+          ".notice-flash": {
+            "background-color": "#45aeeb",
+            "border-color": "#0e5e96",
+            color: "#0e5e96",
+          },
+          ".notice-flash-hover": {
+            "background-color": "#bfe2f8",
+          },
+        },
+      },
+    ],
   },
 };
